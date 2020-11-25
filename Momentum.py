2330eaa31ffb6c7119a08b2e172c6551ed2f12c5
DeltaKiloz <brandon.demerskeller@gmail.com>
2020-11-25 20:55:07 +0100

    Update Momentum.py

--- Momentum.py
+++ Momentum.py
@@ -30,6 +30,7 @@ from subprocess import Popen
 from rich.console import Console
 import os.path
 import sys
+import csv
 
 # initialize the Console() class
 console = Console()
@@ -45,8 +46,11 @@ timestr = strftime("_%Y%m%d")
 # Sales qtr over qtr > 10%; Current Volume > 200k.
 df = fl.scrape("https://finviz.com/screener.ashx?v=111&f=fa_epsqoq_o10,fa_epsyoy_o10,fa_salesqoq_o10,sh_avgvol_o200,sh_curvol_o200,sh_price_o2,ta_sma200_pa,ta_sma50_pa")
 
-# Formats hte filename and creates the .csv file the data will be written to
+# Formats the filenames and creates the .csv files the program will use.
 file = "High-Momo-Stocks" + timestr + ".csv"
+tickerfile = "High-Momo-Stocks" + timestr + "_ticker.csv"
+temp = "temp.csv"
+
 
 # Check to see if the file already exists, that way it won't be over written. If it does exist, you need to either
 # move or delte the file from the current directory so that the new file can be created and written to.
@@ -56,13 +60,12 @@ if os.path.isfile(file) == True:
     sys.exit("[] Exiting program.")
 
 # Notifying user what the .csv filename will be.
-console.print("[]", style = "red")
-console.print("[] Writing data to: ", file, style="red")
+console.print("[] ...", style = "red")
 df.to_csv(file, mode="a", index=False)
 
 # Because the webpage will only allow 20 rows to be viewed at a time with a free account, this
 # will iterate through each page of 20 rows and will be returned from your original query. With
-# each iteration, it will append the data to the same .csv file that was originall created.
+# each iteration, it will append the data to the same .csv file that was originally created.
 count = 0
 while (len(df.index) > 1):
     count = count + 2
@@ -74,6 +77,41 @@ while (len(df.index) > 1):
     df.to_csv(file, mode="a", header=False, index=False)
 
 console.print("[] Scraping completed!", style = "green")
+console.print("[] ...", style = "red")
+console.print("[] Writing full details to file: ", file, style="red")
 
-# Open the .csv file in Excel to inspect the results.
+
+# This next bit works on pulling only the ticker column out of the .csv file for easier import.
+with open(file, 'r') as f:
+    with open(temp, 'w') as f1:
+        next(f) # skip header line
+        for line in f:
+            f1.write(line)
+
+cols_to_remove = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10] # Column indexes to be removed (starts at 0)
+
+cols_to_remove = sorted(cols_to_remove, reverse=True) # Reverse so we remove from the end first
+row_count = 0 # Current amount of rows processed
+
+with open(temp, "r") as source:
+    reader = csv.reader(source)
+    with open(tickerfile, "w", newline='') as result:
+        writer = csv.writer(result)
+        for row in reader:
+            row_count += 1
+            #print('\r{0}'.format(row_count), end='') # Print rows processed
+            for col_index in cols_to_remove:
+                del row[col_index]
+            writer.writerow(row)
+
+# Clean-up (delete) of the temp.csv file  that was used as a placeholder.
+os.remove(temp)
+console.print("[] Writing ticker only results to file: ", tickerfile, style="red")
+console.print("[] ...", style = "red")
+console.print("[] Program complete, good luck investing!", style = "green")
+
+
+# Open the .csv files in Excel to inspect the results.
 Popen(file, shell=True)
+sleep(0.05)
+Popen(tickerfile, shell=True)
